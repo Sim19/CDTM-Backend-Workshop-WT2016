a3aa1eb56d7f9a70ee7f9750824f683e046da82b
Simone Lederer <slederer@cs.ru.nl>
2016-12-18 10:26:33 +0100

    first database with cookies

--- src/exercises/4-Database-Challenges/schema.sql
+++ src/exercises/4-Database-Challenges/schema.sql
@@ -6,3 +6,11 @@ CREATE TABLE Users(
   name        TEXT         NOT NULL UNIQUE,
   created     TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+DROP TABLE IF EXISTS Cookies;
+CREATE TABLE Cookies(
+    id      INTEGER        PRIMARY KEY AUTOINCREMENT,
+    owner   INTEGER        NOT NULL,
+    type    TEXT           NOT NULL DEFAULT 'Zimtstern',
+    FOREIGN KEY(owner) REFERENCES Users(id)
+    );
